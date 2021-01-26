305f9236ebeb2cb76ec68cd750c00c2747ae4e5c
theRealAyan <ayanispro420@gmail.com>
2021-01-26 16:52:15 +0600

    added command: ping.ts

---
+++ src/main.ts
@@ -0,0 +1,53 @@
+const sTime: number = new Date().getTime();
+
+import * as logger from '@ayanthedev/colorlogs';
+import * as config from './config';
+import * as server from './server';
+import { Client, Message } from 'discord.js';
+import { commands } from './handlers/command';
+import { bgBlue, bgGreen } from 'colorette';
+
+const client: Client = new Client();
+
+async function start(): Promise<void> {
+  logger.warn('starting...');
+  await client.login(config.token);
+};
+
+client.on('message', (message: Message): void => {
+  if(!message.channel) return;
+  logger.info(bgBlue(message.guild.name) + bgGreen(message.author.tag) + message.content);
+  if(!message.content.startsWith(config.prefix)){ return; };
+  if(message.content.startsWith(config.prefix)){
+    const arr: string[] = message.content.split(' ');
+    const cmd: string = arr[0].toLowerCase().split(config.prefix)[1];
+    const args: string[] = arr.slice(1);
+
+	try {
+    	commands.forEach(command => {
+    		if(command.cmd.name == cmd){
+    			return commands.get(command.cmd.name).cmd.run(client, message, args);
+        };
+        if(command.cmd.aliases){
+          command.cmd.aliases.forEach((alias) => {
+            if(alias == cmd){
+              return commands.get(command.cmd.name).cmd.run(client, message, args);
+            };
+          });
+        };
+      });
+    } catch(err) {
+      logger.error(err);
+    	logger.error('unable to find/run command: ' + cmd);
+    };
+  };
+});
+
+client.once('ready', (): void => {
+  logger.success(client.user.tag + ' is online');
+  server.start(6969);
+  const eTime: number = new Date().getTime();
+  logger.info('took ' + (eTime - sTime) + 'ms to boot up');
+});
+
+start();
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,15 @@
 import { createServer, IncomingMessage, ServerResponse } from 'http';
+import { success, error } from '@ayanthedev/colorlogs';
 
-export function start(){
-  const port = 6969;
+export function start(port: number){
   const server = createServer((request: IncomingMessage, response: ServerResponse) => {
     response.end('Online.');
   });
-  server.listen(port, () => {});
+  try {
+    server.listen(port, () => {
+      success('server started, port: ' + port);
+    });
+  } catch (err) {
+    error(err);
+  };
 };
