c289feda3f0a60c320c1e01fabf05e390ba89fe6
theRealAyan <ayanispro420@gmail.com>
2021-01-26 16:16:21 +0600

    idk

---
+++ src/server.ts
@@ -0,0 +1,9 @@
+import { createServer, IncomingMessage, ServerResponse } from 'http';
+
+export function start(){
+  const port = 6969;
+  const server = createServer((request: IncomingMessage, response: ServerResponse) => {
+    response.end('Online.');
+  });
+  server.listen(port, () => {});
+};
